966523e96ae8f42bacbcc25016ef5dd4365a8475
Danil Kladnitsky <danyakladnitsky@gmail.com>
2025-04-18 16:46:53 +0300

    frontend: Add linting fix command and improve code formatting across multiple files. Update package.json to include a lint:fix script, and refactor App.context.tsx, App.tsx, and InsertWalletPage.tsx for consistent styling and readability.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,30 @@
-import { ThemeProvider } from '@gravity-ui/uikit';
+import {ThemeProvider} from '@gravity-ui/uikit';
 import bem from 'bem-cn-lite';
 
 import {DEFAULT_THEME} from './constants';
-import { InsertWalletPage } from './pages/InsertWalletPage/InsertWalletPage';
+import {InsertWalletPage} from './pages/InsertWalletPage/InsertWalletPage';
 
 import './main.scss';
-import { AppContextProvider } from './App.context';
-import { Header } from './components/Header/Header';
+import {AppContextProvider} from './App.context';
+import {Header} from './components/Header/Header';
 
 interface AppWrapperProps {
     children: React.ReactNode;
 }
 
-const b = bem('app')
+const b = bem('app');
 
 const AppWrapper = ({children}: AppWrapperProps) => {
     return (
         <AppContextProvider>
             <ThemeProvider theme={DEFAULT_THEME}>
                 <div className={b()}>
-                    <div className={b('wrapper')}>
-                    {children}
-                    </div>
+                    <div className={b('wrapper')}>{children}</div>
                 </div>
             </ThemeProvider>
         </AppContextProvider>
     );
-}
+};
 
 const App = () => {
     return (
--- frontend/src/pages/InsertWalletPage/InsertWalletPage.tsx
+++ frontend/src/pages/InsertWalletPage/InsertWalletPage.tsx
@@ -1,28 +1,38 @@
-import { Text, TextInput, Button, Icon } from '@gravity-ui/uikit'
+import {Button, Icon, Text, TextInput} from '@gravity-ui/uikit';
 import bem from 'bem-cn-lite';
 
 import './styles.scss';
-import { CreditCard } from '@gravity-ui/icons';
-import { useAppContext } from '@/App.context';
+import {CreditCard} from '@gravity-ui/icons';
+import {useAppContext} from '@/App.context';
 
 const b = bem('insert-wallet-page');
 
 export const InsertWalletPage = () => {
-  const { walletAddress, setWalletAddress, searchWallet } = useAppContext();
+    const {walletAddress, setWalletAddress, searchWallet} = useAppContext();
 
-  const handleSearch = () => {
-    searchWallet();
-  }
+    const handleSearch = () => {
+        searchWallet();
+    };
 
-  return (
-    <div className={b()}>
-      <div className={b('form')}>
-        <Text variant='body-3' className={b('form-title')}>Provide existing wallet address <Icon size={20} data={CreditCard} /></Text>
-        <div className={b('form-input')}>
-          <TextInput placeholder='Wallet address in hex format' size='xl' hasClear value={walletAddress} onChange={(e) => setWalletAddress(e.target.value)} />
-          <Button size='xl' view='action' onClick={handleSearch}>Search</Button>
+    return (
+        <div className={b()}>
+            <div className={b('form')}>
+                <Text variant="body-3" className={b('form-title')}>
+                    Provide existing wallet address <Icon size={20} data={CreditCard} />
+                </Text>
+                <div className={b('form-input')}>
+                    <TextInput
+                        placeholder="Wallet address in hex format"
+                        size="xl"
+                        hasClear
+                        value={walletAddress}
+                        onChange={(e) => setWalletAddress(e.target.value)}
+                    />
+                    <Button size="xl" view="action" onClick={handleSearch}>
+                        Search
+                    </Button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
